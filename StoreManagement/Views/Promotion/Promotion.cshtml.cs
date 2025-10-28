0d544da904e63db45d957c3c2c6454de3f57bad4
TrongHiuuu <nthieu.c4pbchau2122@gmail.com>
2025-10-29 04:15:15 +0700

    29/10/2025
    init

---
+++ StoreManagement/Views/Promotion/Promotion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StoreManagement.Views.Promotion
+{
+    public class PromotionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
