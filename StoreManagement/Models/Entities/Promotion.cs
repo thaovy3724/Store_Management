4f0ed1f33e354c0f869147c8f1b81592f665a2bd
TrongHiuuu <nthieu.c4pbchau2122@gmail.com>
2025-10-29 04:18:30 +0700

    29/10/2025 - Chưa xong
    Entity Model Mã giảm giá

--- StoreManagement/Models/Entities/Promotion.cs
+++ StoreManagement/Models/Entities/Promotion.cs
@@ -2,11 +2,15 @@
 {
     public class Promotion
     {
-        public int PromotionId { get; set; }
-        public string PromotionCode { get; set; }
-        public string Description { get; set; }
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Type { get; set; } // "percent" hoặc "fixed"
+        public decimal Value { get; set; }
+        public decimal MinOrderAmount { get; set; }
+        public int UsageLimit { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public 
+        public bool IsLocked { get; set; } = false;
     }
+
 }
