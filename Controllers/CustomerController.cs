208bf2f02760b2e77a9aa6b05244d6ca5abdfb8b
Trong-123 <trongngo.02092004@gmail.com>
2025-12-16 08:10:28 +0700

    Chinh lai phan trang va san pham

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -126,7 +126,7 @@ public class CustomerController(ApplicationDbContext _dbContext) : Controller
     }
     
     // POST - Customer/Edit/{id}
-    [HttpPut]
+    [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Edit(int id, Customer model)
     {
